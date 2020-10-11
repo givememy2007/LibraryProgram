a61df3ecbc042864ca7b58b0b242da1fcd3d96fd
givememy2007 <kortin95@mail.ru>
2020-10-11 09:32:04 +0700

    Создание программы и БД.

---
+++ LibraryProgram/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [BookName] NVARCHAR(50) NOT NULL, 
+    [AutorName] NVARCHAR(50) NOT NULL, 
+    [PublisherName] NVARCHAR(50) NOT NULL, 
+    [PublishingYear] NVARCHAR(50) NOT NULL, 
+    [Availability] INT NOT NULL DEFAULT 1
+)
